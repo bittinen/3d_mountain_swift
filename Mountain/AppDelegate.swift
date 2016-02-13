b095677a62f914dfcd95cb1351e4deb3da015843
Mika Leppinen <bittinen@gmail.com>
2016-02-13 15:23:51 +0200

    Initial Commit

---
+++ Mountain/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Mountain
+//
+//  Created by bittinen on 13/02/16.
+//  Copyright (c) 2016 Mika Leppinen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+    
+}
