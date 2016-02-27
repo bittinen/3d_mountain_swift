b8221768c096d2da51e48e7b63496558ca426236
Mika Leppinen <bittinen@gmail.com>
2016-02-27 18:16:23 +0200

    Recursion works

--- Mountain/GameViewController.swift
+++ Mountain/GameViewController.swift
@@ -15,9 +15,9 @@ class GameViewController: NSViewController {
     var norms = [SCNVector3]()
     var verts = [SCNVector3]()
     var colors = [SCNVector3]()
-    var first = true;
-    let size = 4
+    let size = 6
     var primitives = 0
+    
     
     internal func calcuN(A:SCNVector3, B:SCNVector3, C:SCNVector3)->SCNVector3
     {
@@ -36,13 +36,12 @@ class GameViewController: NSViewController {
         return SCNVector3((A.x+B.x+C.x)/3,(A.y+B.y+C.y)/3,(A.z+B.z+C.z)/3)
     }
     
-    internal func re(A:SCNVector3, B:SCNVector3, C:SCNVector3)
+    internal func re(A:SCNVector3, B:SCNVector3, C:SCNVector3, levels: Int)
     {
         
-        var y = 0
-        if first {
-            first = false
-            y = size;
+        var y = CGFloat(0)
+        if levels == 0 {
+            y = CGFloat(size)
             verts.append(A)
             verts.append(C)
             verts.append(B)
@@ -50,62 +49,73 @@ class GameViewController: NSViewController {
             norms.append(n)
             norms.append(n)
             norms.append(n)
-            colors.append(SCNVector3(0.0, 0.1, 0.0))
-            colors.append(SCNVector3(0.0, 0.1, 0.0))
-            colors.append(SCNVector3(0.0, 0.1, 0.0))
+            colors.append(SCNVector3(1.0, 0.0, 0.0))
+            colors.append(SCNVector3(1.0, 0.0, 0.0))
+            colors.append(SCNVector3(1.0, 0.0, 0.0))
             primitives++
-
         }
-        let p1 = CGFloat((arc4random_uniform(20)+90)/100)
-        let p2 = CGFloat((arc4random_uniform(20)+90)/100)
+        else {
+            let T = calcuC(A,B:B,C:C)
+            y = T.y
+        }
+        
+        let p1 = CGFloat((CGFloat(arc4random_uniform(40))+80.0)/100.0)
+        let p2 = CGFloat((CGFloat(arc4random_uniform(40))+80.0)/100.0)
         var P = calcuC(A,B:B,C:C)
         P.x = P.x*p1
-        P.y = P.y*p2
-        P.z = CGFloat(y);
+        P.z = P.z*p2
+        P.y = CGFloat(y);
         
-        verts.append(A)
-        verts.append(B)
-        verts.append(P)
-        let n1 = calcuN(A,B:B,C:P)
-        norms.append(n1)
-        norms.append(n1)
-        norms.append(n1)
-        colors.append(SCNVector3(0.0, 0.0, 1.0))
-        colors.append(SCNVector3(0.0, 0.0, 1.0))
-        colors.append(SCNVector3(1.0, 0.0, 1.0))
-        primitives++
+        if levels < 5 {
+            re(A, B:B, C:P, levels:(levels+1)) // A, B, P
+            re(A, B:P, C:C, levels:(levels+1)) // A, C, P
+            re(P, B:B, C:C, levels:(levels+1)) // B, C, P
+        }
+        else {
+            verts.append(P)
+            verts.append(A)
+            verts.append(B)
+            let n1 = calcuN(A,B:B,C:P)
+            norms.append(n1)
+            norms.append(n1)
+            norms.append(n1)
+            colors.append(SCNVector3(3.0*P.y/CGFloat(size), 1.6, 0.5))
+            colors.append(SCNVector3(3.0*A.y/CGFloat(size), 1.6, 0.5))
+            colors.append(SCNVector3(3.0*B.y/CGFloat(size), 0.6, 0.5))
+            primitives++
         
-        verts.append(A)
-        verts.append(P)
-        verts.append(C)
-        let n2 = calcuN(A,B:P,C:C)
-        norms.append(n2)
-        norms.append(n2)
-        norms.append(n2)
-        colors.append(SCNVector3(0.0, 0.0, 1.0))
-        colors.append(SCNVector3(1.0, 0.0, 1.0))
-        colors.append(SCNVector3(0.0, 0.0, 1.0))
-        primitives++
+            verts.append(A)
+            verts.append(P)
+            verts.append(C)
+            let n2 = calcuN(A,B:P,C:C)
+            norms.append(n2)
+            norms.append(n2)
+            norms.append(n2)
+            colors.append(SCNVector3(3.0*A.y/CGFloat(size), 1.6, 0.5))
+            colors.append(SCNVector3(3.0*P.y/CGFloat(size), 1.6, 0.5))
+            colors.append(SCNVector3(3.0*C.y/CGFloat(size), 0.6, 0.5))
+            primitives++
         
-        verts.append(P)
-        verts.append(B)
-        verts.append(C)
-        let n3 = calcuN(P,B:B,C:C)
-        norms.append(n3)
-        norms.append(n3)
-        norms.append(n3)
-        colors.append(SCNVector3(1.0, 0.0, 1.0))
-        colors.append(SCNVector3(0.0, 0.0, 1.0))
-        colors.append(SCNVector3(0.0, 0.0, 1.0))
-        primitives++
+            verts.append(P)
+            verts.append(B)
+            verts.append(C)
+            let n3 = calcuN(P,B:B,C:C)
+            norms.append(n3)
+            norms.append(n3)
+            norms.append(n3)
+            colors.append(SCNVector3(3.0*P.y/CGFloat(size), 1.6, 0.5))
+            colors.append(SCNVector3(3.0*B.y/CGFloat(size), 1.6, 0.5))
+            colors.append(SCNVector3(3.0*C.y/CGFloat(size), 0.6, 0.5))
+            primitives++
+        }
     }
     
     override func awakeFromNib(){
         // create a new scene
         let a = SCNVector3(x: 0,y: 0,z: 0)
         let b = SCNVector3(x: CGFloat(size),y: 0,z: 0)
-        let c = SCNVector3(x: CGFloat(size/2),y: CGFloat(size),z: 0)
-        re(a,B:b,C:c)
+        let c = SCNVector3(x: CGFloat(size/2),y: 0,z: CGFloat(-size))
+        re(a,B:b,C:c, levels:0)
 
         let src = SCNGeometrySource(vertices: &verts, count: verts.count)
         
