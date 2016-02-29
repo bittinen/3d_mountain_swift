c05e48d4dce525745651833348c9ee83a7a40718
Mika Leppinen <bittinen@gmail.com>
2016-02-29 17:08:45 +0200

    typo fix

--- Mountain/GameViewController.swift
+++ Mountain/GameViewController.swift
@@ -9,7 +9,7 @@
 //  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
 //  IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-// Warning: This was my fist Swift project
+// Warning: This was my first Swift project
 import SceneKit
 import QuartzCore
 
